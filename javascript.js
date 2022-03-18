675c986c2bb92ef3d970de8036951fc8f48cdd5c
thatsMind <collinzizhen@gmail.com>
2022-03-18 15:59:44 -0700

    stylize the page,add color to the hands, and start over button.

--- javascript.js
+++ javascript.js
@@ -16,18 +16,15 @@ function computerPlay(){
 function playRound(playerSelection, computerSelection){
     let playerSelectionFix = playerSelection.toLowerCase()
 
-    const div6 = document.querySelector(".comparison")
-    div6.textContent = `Player: ${playerSelectionFix}. Computer: ${computerSelection}`;
-
     switch(playerSelection){
         case "rock":
-            content2.src = "./img/rock.png"
+            content2.src = "./img/prock.png"
             break;
         case "paper":
-            content2.src = "./img/paper.png"
+            content2.src = "./img/ppaper.png"
             break;
         case "scissor":
-            content2.src = "./img/scissor.png"
+            content2.src = "./img/pscissor.png"
             break;
         default:
             content2.src = "./img/blank.png"
@@ -35,13 +32,13 @@ function playRound(playerSelection, computerSelection){
 
     switch(computerSelection){
         case "rock":
-            content6.src = "./img/rock.png"
+            content6.src = "./img/crock.png"
             break;
         case "paper":
-            content6.src = "./img/paper.png"
+            content6.src = "./img/cpaper.png"
             break;
         case "scissor":
-            content6.src = "./img/scissor.png"
+            content6.src = "./img/cscissor.png"
             break;
         default:
             content6.src = "./img/blank.png"
@@ -49,52 +46,52 @@ function playRound(playerSelection, computerSelection){
 
 
     if (playerSelectionFix === computerSelection){
-        const div5 = document.querySelector(".result")
+        const div5 = document.querySelector(".round")
         content2.style.border = "2px solid black";
         content6.style.border = "2px solid black";
         div5.textContent ="It's a tie!";
         return "tie"
 
     } else if (playerSelectionFix==="rock"&&computerSelection==="scissor"){
-        const div5 = document.querySelector(".result")
+        const div5 = document.querySelector(".round")
         content2.style.border = "2px solid green";
         content6.style.border = "2px solid red";
 
-        div5.textContent ="Player won!"
+        div5.textContent ="Player wins!"
         return "player"
     } else if (playerSelectionFix==="rock"&&computerSelection==="paper"){
-        const div5 = document.querySelector(".result")
+        const div5 = document.querySelector(".round")
         content6.style.border = "2px solid green";
         content2.style.border = "2px solid red";
-        div5.textContent ="Computer won!"
+        div5.textContent ="Computer wins!"
         return "computer"
 
     }else if (playerSelectionFix==="paper"&&computerSelection==="rock"){
-        const div5 = document.querySelector(".result")
+        const div5 = document.querySelector(".round")
         content2.style.border = "2px solid green";
         content6.style.border = "2px solid red";
-        div5.textContent ="Player won!"
+        div5.textContent ="Player wins!"
         return "player"
 
     } else if (playerSelectionFix==="paper"&&computerSelection==="scissor"){
-        const div5 = document.querySelector(".result")
+        const div5 = document.querySelector(".round")
         content6.style.border = "2px solid green";
         content2.style.border = "2px solid red";
-        div5.textContent ="Computer won!"
+        div5.textContent ="Computer wins!"
         return "computer"
 
     }else if (playerSelectionFix==="scissor"&&computerSelection==="paper"){
-        const div5 = document.querySelector(".result")
+        const div5 = document.querySelector(".round")
         content2.style.border = "2px solid green";
         content6.style.border = "2px solid red";
-        div5.textContent ="Player won!"
+        div5.textContent ="Player wins!"
         return "player"
 
     } else if (playerSelectionFix==="scissor"&&computerSelection==="rock"){
-        const div5 = document.querySelector(".result")
+        const div5 = document.querySelector(".round")
         content6.style.border = "2px solid green";
         content2.style.border = "2px solid red";
-        div5.textContent ="Computer won!"
+        div5.textContent ="Computer wins!"
         return "computer"
 
     } else{
@@ -110,10 +107,10 @@ function addScore(string){
     }
 
     const score1 =document.querySelector(".score1")
-    score1.textContent = `Player Score: ${playerScore}`;
+    score1.textContent = `${playerScore}`;
 
     const score2 =document.querySelector(".score2")
-    score2.textContent = `Computer Score: ${computerScore}`;
+    score2.textContent = `${computerScore}`;
     
 }
 
@@ -122,9 +119,25 @@ function checkWinner(){
 
     if (playerScore>=5){
         winner.textContent ="Player is the winner!"
+        return true
     } else if (computerScore>=5){
         winner.textContent ="Computer is the winner!"
+        return true
     }
+    return false;
+}
+
+function reset(){
+    content2.src = "./img/blank.png"            
+    content6.src = "./img/blank.png"
+    content2.style.border="2px solid black"       
+    content6.style.border="2px solid black"       
+    playerScore=0
+    computerScore =0
+    content3.textContent = `${computerScore}`;
+    content7.textContent = `${computerScore}`;
+    div5.textContent = ``;
+    div7.textContent = ``;
 }
 
 const btn =document.querySelector("#rock");
@@ -132,32 +145,36 @@ const btn2 =document.querySelector("#paper");
 const btn3 =document.querySelector("#scissor");
 let playerScore=0;
 let computerScore=0;
-let result="yo";
+let round="";
 
-const body = document.querySelector("body")
-
-const buttons = document.querySelector(".buttons")
+const body = document.querySelector("body");
+const buttons = document.querySelector(".buttons");
 
 
 
 
 btn.addEventListener("click",()=>{
-    result=playRound("rock", computerPlay())
-    addScore(result);
-    checkWinner();
+    if (!checkWinner()){
+        round=playRound("rock", computerPlay())
+        addScore(round);
+        checkWinner()
+    }
 });
 
 btn2.addEventListener("click",()=>{
-    result=playRound("paper", computerPlay())
-    addScore(result);
-    checkWinner();
-
+    if (!checkWinner()){
+        round=playRound("paper", computerPlay())
+        addScore(round);
+        checkWinner()
+    }
 });
 
 btn3.addEventListener("click",()=>{
-    result=playRound("scissor", computerPlay())
-    addScore(result);
-    checkWinner();
+    if (!checkWinner()){
+        round=playRound("scissor", computerPlay())
+        addScore(round);
+        checkWinner()
+    }
 
 });
 const div1 = document.createElement("h1");
@@ -176,10 +193,10 @@ body.appendChild(div4);
 
 
     const innerDiv1 = document.createElement("div");
-    innerDiv1.style.cssText = "display:flex; flex-direction:column"
+    innerDiv1.style.cssText = "display:flex; flex-direction:column; align-items:center"
     div4.appendChild(innerDiv1);
 
-        const content1 = document.createElement("div");
+        const content1 = document.createElement("h3");
         content1.textContent="PLAYER";
         innerDiv1.appendChild(content1);
 
@@ -188,9 +205,9 @@ body.appendChild(div4);
         content2.style.cssText="transform: rotate(90deg);border: 2px solid black; border-radius: 25px; height: 160px; width:160px";
         innerDiv1.appendChild(content2);
 
-        const content3 = document.createElement("div");
+        const content3 = document.createElement("h1");
         content3.classList.add("score1")
-        content3.textContent = `Score: ${playerScore}`;
+        content3.textContent = `${playerScore}`;
         innerDiv1.appendChild(content3);
 
 
@@ -199,15 +216,15 @@ body.appendChild(div4);
     div4.appendChild(innerDiv2);
 
         const content4 = document.createElement("div");
-        content4.textContent="Game #/#";
+        content4.textContent="VS";
         innerDiv2.appendChild(content4);
 
 
     const innerDiv3 = document.createElement("div");
-    innerDiv3.style.cssText = "display:flex; flex-direction:column;align-items:flex-end;"
+    innerDiv3.style.cssText = "display:flex; flex-direction:column;align-items:center;"
     div4.appendChild(innerDiv3);
 
-        const content5 = document.createElement("div");
+        const content5 = document.createElement("h3");
         content5.textContent="COMPUTER";
         innerDiv3.appendChild(content5);
 
@@ -216,27 +233,29 @@ body.appendChild(div4);
         content6.style.cssText="transform: rotate(-90deg);border: 2px solid black; border-radius: 25px; height: 160px; width:160px";
         innerDiv3.appendChild(content6);
 
-        const content7 = document.createElement("div");
+        const content7 = document.createElement("h1");
         content7.classList.add("score2")
-        content7.textContent = `Score: ${computerScore}`;
+        content7.textContent = `${computerScore}`;
         innerDiv3.appendChild(content7);
 
 
 
 const div5 = document.createElement("div");
-div5.classList.add("result")
+div5.classList.add("round")
 div5.textContent = ``;
 
-const div6 = document.createElement("div");
-div6.classList.add("comparison")
-div6.textContent = `Player: none. Computer: none`;
-
-
-
-const div7 = document.createElement("div");
+const div7 = document.createElement("h1");
 div7.classList.add("winner")
 div7.textContent = ``;
 
+const div8 = document.createElement("button");
+div8.textContent = "Start Over";
+
+div8.addEventListener("click",()=>{
+    reset();
+});
+
 body.appendChild(div5);
-body.appendChild(div6);
 body.appendChild(div7);
+body.appendChild(div8);
+
